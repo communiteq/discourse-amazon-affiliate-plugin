f8276e9a66573c30f0cc6971a59299ee8ab44d7a
communiteq <support@discoursehosting.com>
2026-08-26 00:37:30 +0200

    Update ffi-compiler gem version to 1.3.2

--- plugin.rb
+++ plugin.rb
@@ -14,7 +14,7 @@ AmazonAffiliate.install_gems
 gem 'domain_name', '0.5.20190701'
 gem 'http-cookie', '1.0.3'
 gem 'http-form_data', '2.3.0', require: true, require_name: 'http/form_data'
-gem 'ffi-compiler', '1.0.1', require: true, require_name: 'ffi-compiler/loader'
+gem 'ffi-compiler', '1.3.2', require: true, require_name: 'ffi-compiler/loader'
 gem 'http-parser', '1.2.1'
 gem 'http', '4.4.1'
 gem 'vacuum', '3.4.0', require: true
